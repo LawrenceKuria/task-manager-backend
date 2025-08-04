ef3600732355d7a7a4469a512287b47297733358
Lawrence Kuria <i3uyer2@gmail.com>
2025-08-04 00:11:24 -0700

    Added all endpoints for task lists

--- src/main/java/com/lawrence/task_manager/controllers/TaskListController.java
+++ src/main/java/com/lawrence/task_manager/controllers/TaskListController.java
@@ -7,6 +7,8 @@ import com.lawrence.task_manager.services.TaskListService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/task-lists")
@@ -28,10 +30,27 @@ public class TaskListController {
                 .toList();
     }
 
+    @GetMapping(path = "/{task_list_id}")
+    public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id") UUID taskListId) {
+        return taskListService.getTaskList(taskListId).map(taskListMapper::toDto);
+    }
+
     @PostMapping
     public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
         TaskList createdTaskList = taskListService.createTaskList(taskListMapper.fromDto(taskListDto));
 
         return taskListMapper.toDto(createdTaskList);
+    }
+
+    @PutMapping(path = "/{task_list_id}")
+    public TaskListDto updateTaskList(@PathVariable("task_list_id") UUID taskListId, @RequestBody TaskListDto taskListDto) {
+        TaskList updatedTaskList = taskListService.updateTaskList(taskListId, taskListMapper.fromDto(taskListDto));
+
+        return taskListMapper.toDto(updatedTaskList);
+    }
+
+    @DeleteMapping(path = "/{task_list_id}")
+    public void deleteTaskList(@PathVariable("task_list_id") UUID taskListId) {
+        taskListService.deleteTaskList(taskListId);
     }
 }
--- src/main/java/com/lawrence/task_manager/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/lawrence/task_manager/services/impl/TaskListServiceImpl.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TaskListServiceImpl implements TaskListService {
@@ -41,5 +44,35 @@ public class TaskListServiceImpl implements TaskListService {
                 now,
                 now
         ));
+    }
+
+    @Override
+    public Optional<TaskList> getTaskList(UUID id) {
+        return taskListRepository.findById(id);
+    }
+
+    @Override
+    public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
+        if (null == taskList.getId()) {
+            throw new IllegalArgumentException("Task list must have an ID!");
+        }
+
+        if (!Objects.equals(taskList.getId(), taskListId)) {
+            throw new IllegalArgumentException("You cannot change the task list id!");
+        }
+
+        TaskList existingTaskList = taskListRepository.findById(taskListId)
+                .orElseThrow(() -> new IllegalArgumentException("Task list not found"));
+
+        existingTaskList.setTitle(taskList.getTitle());
+        existingTaskList.setDescription(taskList.getDescription());
+        existingTaskList.setUpdated(LocalDateTime.now());
+
+        return taskListRepository.save(existingTaskList);
+    }
+
+    @Override
+    public void deleteTaskList(UUID taskListId) {
+        taskListRepository.deleteById(taskListId);
     }
 }
